e266b42725d1781e90ffed9f47d06bd61360d618
leishi <snow_1015@outlook.com>
2016-05-30 12:32:27 -0700

    passed basic test

--- src/shardmaster/client.go
+++ src/shardmaster/client.go
@@ -18,6 +18,12 @@ type Clerk struct {
 	
 	currId int
 	currIdMu *sync.Mutex
+	
+	rpcRequest chan interface{}
+	rpcMap map[int]chan interface{}
+	rpcMu *sync.Mutex
+	
+	leaderId int
 }
 
 func nrand() int64 {
@@ -40,6 +46,14 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck.currId = 0
 	ck.currIdMu = &sync.Mutex{}
 	
+	ck.rpcRequest = make(chan interface{})
+	ck.rpcMap = make(map[int]chan interface{})
+	ck.rpcMu = &sync.Mutex{}
+	
+	ck.leaderId = -1
+	
+	go ck.ProcessRpcRequest()
+		
 	return ck
 }
 
@@ -53,27 +67,205 @@ func (ck *Clerk) NewID() int {
 	return newId
 }
 
+
+func (ck *Clerk) WrongLeader(reply interface{}) (wrongLeader bool, err Err) {
+	
+	switch replyType := reply.(type) {
+		case JoinReply:
+			r := reply.(JoinReply)
+			wrongLeader = r.WrongLeader
+			err = r.Err
+			
+		case LeaveReply:	
+			r := reply.(LeaveReply)
+			wrongLeader = r.WrongLeader
+			err = r.Err
+		
+		case MoveReply:
+			r := reply.(MoveReply)
+			wrongLeader = r.WrongLeader
+			err = r.Err
+
+		case QueryReply:
+			r := reply.(QueryReply)
+			wrongLeader = r.WrongLeader
+			err = r.Err
+						
+		default:
+			fmt.Printf("client: unknown reply type %v\n", replyType)
+	}
+	
+	return		
+}
+
+func (ck *Clerk) RpcCall(server *labrpc.ClientEnd, args interface{}) (ok bool, reply interface{}){
+	switch argsType := args.(type) {
+			case JoinArgs:
+				rpcName := "ShardMaster.Join"
+				r := JoinReply{}
+				joinArgs := args.(JoinArgs)
+				ok = server.Call(rpcName, &joinArgs, &r)
+				reply = r
+				
+			case LeaveArgs:	
+				rpcName := "ShardMaster.Leave"
+				r := LeaveReply{}
+				leaveArgs := args.(LeaveArgs)
+				
+				ok = server.Call(rpcName, &leaveArgs, &r)
+				reply = r
+
+			case MoveArgs:	
+				rpcName := "ShardMaster.Move"
+				r := MoveReply{}
+				moveArgs := args.(MoveArgs)
+				
+				ok = server.Call(rpcName, &moveArgs, &r)
+				reply = r
+				
+			case QueryArgs:	
+				rpcName := "ShardMaster.Query"
+				r := QueryReply{}
+				queryArgs := args.(QueryArgs)
+				
+				ok = server.Call(rpcName, &queryArgs, &r)
+				reply = r
+								
+			default:
+				fmt.Printf("client: RpcCall, unknown args %v type %v\n", args, argsType)
+				return
+		}
+		
+		return
+}
+
+func (ck *Clerk) ProcessRpcRequest() {
+	
+	for {
+		
+		args:=<-ck.rpcRequest
+		var reply interface{}
+		var id int
+		
+		switch argsType := args.(type) {
+			case JoinArgs:
+				id = args.(JoinArgs).ID
+				
+			case LeaveArgs:	
+				id = args.(LeaveArgs).ID
+
+			case MoveArgs:
+				id = args.(MoveArgs).ID
+				
+			case QueryArgs:	
+				id = args.(QueryArgs).ID
+							
+			default:
+				fmt.Printf("client: unknown args %v type %v\n", args, argsType)
+				continue
+		}
+		
+		for {
+			// fmt.Printf("client [%v], leaderID %v\n", ck.id, ck.leaderId)
+			if(ck.leaderId == -1) {
+				
+				for i := 0;i<len(ck.servers);i++ {
+					ok, r := ck.RpcCall(ck.servers[i], args)
+					reply = r
+					
+					//fmt.Printf("----> client, %v Get ok %v reply %v\n", 
+					//	i, ok, reply)
+					
+					wrongLeader, _ := ck.WrongLeader(reply)
+					if(!ok || wrongLeader) {
+						time.Sleep(500 * time.Millisecond)
+						continue
+					}
+					
+					fmt.Printf("== client, leader changed to %v \n", i)
+					ck.leaderId = i
+					
+					break
+				}
+			} else {
+				
+				success := false
+				for retry := 0;retry < 5; retry ++ {
+					ok, r := ck.RpcCall(ck.servers[ck.leaderId], args)
+					reply = r
+					
+					if(!ok) {
+						time.Sleep(500 * time.Millisecond)
+						continue
+					}
+					
+					wrongLeader, _ := ck.WrongLeader(reply)
+					
+					if(wrongLeader) {
+						fmt.Printf("== client, wrong leader %v\n", ck.leaderId)
+						ck.leaderId = -1
+						
+						break;
+					}
+					
+					// ck.leaderId = serverId
+					success = true
+					break
+				}
+				
+				if(!success){
+					ck.leaderId = -1
+				}
+			}
+			
+			if(ck.leaderId != -1) {
+				ck.rpcMap[id] <- reply
+				
+				break
+			}
+			
+			time.Sleep(100 * time.Millisecond)
+		}
+	}	
+}
+
+func (ck *Clerk) RpcCallAndWait(id int, args interface{}) (reply interface{}) {
+	
+	
+	ch := make(chan interface{})
+	
+	ck.rpcMu.Lock()
+	ck.rpcMap[id] = ch
+	ck.rpcMu.Unlock()
+	
+	// fmt.Printf("client: enqueue rpc request %v \n", args)
+	ck.rpcRequest <- args
+	
+	r:=<-ch
+	reply = r
+	
+	return
+}
+
 func (ck *Clerk) Query(num int) Config {
 	args := &QueryArgs{}
 	// Your code here.
 	args.Num = num
 	args.ID = ck.NewID()
 	
-	for {
-		// try each known server.
-		for _, srv := range ck.servers {
-			var reply QueryReply
-			ok := srv.Call("ShardMaster.Query", args, &reply)
-			if ok && reply.WrongLeader == false {
-				fmt.Printf("------ client, query Index [%v]=[%v] -----\n", args.ID, reply.Config)
-				return reply.Config
-			}
-			
-			// fmt.Printf("------ client, query server %v, ok %v wrong leader %v -----\n", 
-			//	i, ok, reply.WrongLeader)
-		}
-		time.Sleep(100 * time.Millisecond)
+	r := ck.RpcCallAndWait(args.ID, *args)
+	reply := r.(QueryReply)
+	
+	if(reply.Err != Err_Success) {
+		fmt.Printf(">> client, Failed to query [%v] ,%v\n", 
+			args.Num, reply.Err)
+		
+		return Config{}
 	}
+	
+	fmt.Printf("------ client, query Index [%v]=[%v] -----\n", args.ID, reply.Config)
+	
+	return reply.Config
 }
 
 func (ck *Clerk) Join(servers map[int][]string) {
@@ -82,19 +274,17 @@ func (ck *Clerk) Join(servers map[int][]string) {
 	args.Servers = servers
 	args.ID = ck.NewID()
 	
-	fmt.Printf("------ client, join Index [%v] servers [%v] -----\n", args.ID, args.Servers)
+	r := ck.RpcCallAndWait(args.ID, *args)
+	reply := r.(JoinReply)
 	
-	for {
-		// try each known server.
-		for _, srv := range ck.servers {
-			var reply JoinReply
-			ok := srv.Call("ShardMaster.Join", args, &reply)
-			if ok && reply.WrongLeader == false {
-				return
-			}
-		}
-		time.Sleep(100 * time.Millisecond)
+	if(reply.Err != Err_Success) {
+		fmt.Printf(">> client, Failed to join [%v] ,%v\n", 
+			servers, reply.Err)
+		
+		return
 	}
+	
+	fmt.Printf("------ client, join Index [%v] servers [%v] -----\n", args.ID, servers)
 }
 
 func (ck *Clerk) Leave(gids []int) {
@@ -102,19 +292,18 @@ func (ck *Clerk) Leave(gids []int) {
 	// Your code here.
 	args.GIDs = gids
 	args.ID = ck.NewID()
-	fmt.Printf("------ client, leave Index [%v] servers [%v] -----\n", args.ID, args.GIDs)
 	
-	for {
-		// try each known server.
-		for _, srv := range ck.servers {
-			var reply LeaveReply
-			ok := srv.Call("ShardMaster.Leave", args, &reply)
-			if ok && reply.WrongLeader == false {
-				return
-			}
-		}
-		time.Sleep(100 * time.Millisecond)
+	r := ck.RpcCallAndWait(args.ID, *args)
+	reply := r.(LeaveReply)
+	
+	if(reply.Err != Err_Success) {
+		fmt.Printf(">> client, Failed to leave [%v] ,%v\n", 
+			gids, reply.Err)
+		
+		return
 	}
+	
+	fmt.Printf("------ client, leave Index [%v] servers [%v] -----\n", args.ID, args.GIDs)
 }
 
 func (ck *Clerk) Move(shard int, gid int) {
@@ -122,19 +311,17 @@ func (ck *Clerk) Move(shard int, gid int) {
 	// Your code here.
 	args.Shard = shard
 	args.GID = gid
-	args.ID = ck.NewID()
+	args.ID = ck.NewID()	
+	
+	r := ck.RpcCallAndWait(args.ID, *args)
+	reply := r.(MoveReply)
+	
+	if(reply.Err != Err_Success) {
+		fmt.Printf(">> client, Failed to move [%v] ,%v\n", 
+			gid, reply.Err)
+		
+		return
+	}
 	
 	fmt.Printf("------ client, move Index [%v] [%v]->[%v]-----\n", args.ID, args.Shard, args.GID)
-	
-	for {
-		// try each known server.
-		for _, srv := range ck.servers {
-			var reply MoveReply
-			ok := srv.Call("ShardMaster.Move", args, &reply)
-			if ok && reply.WrongLeader == false {
-				return
-			}
-		}
-		time.Sleep(100 * time.Millisecond)
-	}
 }
--- src/shardmaster/server.go
+++ src/shardmaster/server.go
@@ -166,6 +166,86 @@ func (sm *ShardMaster) CloneGroups() map[int][]string {
 	return cloned
 }
 
+func (sm *ShardMaster) MinMaxLoadGroupID() (minLoadId, maxLoadId, delta int) {
+	
+	minLoadId = 0
+	maxLoadId = 0
+	
+	currConfig := sm.configs[sm.currConfig]
+	shards := currConfig.Shards
+	groups := currConfig.Groups
+	
+	loadMap := make(map[int]int)
+	for gid := range groups {
+		loadMap[gid] = 0	
+	} 
+	
+	for _, gid := range shards{
+		if(gid != 0){
+			loadMap[gid] ++
+		}
+	}
+	
+	minLoad := NShards + 1
+	maxLoad := 0
+	
+	for gid, load := range loadMap {
+		if(load <= minLoad) {
+			minLoadId = gid
+			minLoad = loadMap[gid]
+		}
+		
+		if(load >= maxLoad){
+			maxLoadId = gid
+			maxLoad = loadMap[gid]
+		}
+	}
+	
+	delta = loadMap[maxLoadId] - loadMap[minLoadId]
+	// fmt.Printf("  >> server, min [%v] max [%v] delta [%v] <<\n", minLoadId, maxLoadId, delta)
+	return
+}
+
+func (sm *ShardMaster) BalanceShardsForJoin() {
+	
+	currConfig := sm.configs[sm.currConfig]
+	if(len(currConfig.Groups) == 0) {
+		fmt.Printf("ERROR, server no groupID\n")
+		return	
+	} 
+	
+	min, max, delta := sm.MinMaxLoadGroupID()
+	
+	for(delta > 1) {
+		
+		for i := 0;i<len(currConfig.Shards);i++{
+			if(currConfig.Shards[i] == max){
+				currConfig.Shards[i] = min
+				break
+			}
+		}
+		sm.configs[sm.currConfig] = currConfig
+		min, max, delta = sm.MinMaxLoadGroupID()
+	}
+}
+
+func (sm *ShardMaster) BalanceShardsForLeave() {
+	
+	currConfig := sm.configs[sm.currConfig]
+	if(len(currConfig.Groups) == 0) {
+		fmt.Printf("ERROR, server no groupID\n")
+		return	
+	} 
+		
+	for i := 0;i<len(currConfig.Shards);i++{
+		if(currConfig.Shards[i] == 0){
+			min, _, _ := sm.MinMaxLoadGroupID()
+			currConfig.Shards[i] = min
+		}
+		sm.configs[sm.currConfig] = currConfig
+	}
+}
+
 func (sm *ShardMaster) ApplyToStateMachine(applyOp Op) (reply interface{}) {
 	
 	// fmt.Printf("#### %v start ApplyToStateMachine %v ####\n", sm.me,applyOp)
@@ -177,39 +257,30 @@ func (sm *ShardMaster) ApplyToStateMachine(applyOp Op) (reply interface{}) {
 	currConfig := sm.configs[sm.currConfig]
 	currGroups := sm.CloneGroups()
 	currShards := currConfig.Shards
-	currGroupLen := len(currGroups)
 			
 	switch applyOp.Code{
 		case Join:
 			serversMap := applyOp.Servers
 			joinReply := JoinReply{}
-
-			if(currGroupLen + len(serversMap) > NShards) {
-				joinReply.Err = Err_InvalidParameter
-				reply = joinReply
-				break
-			}	
 			
 			for groupId, servers := range serversMap {
 				
 				_, ok := currGroups[groupId]
 				
 				if(ok) {
-					log += fmt.Sprintf("server, groupId [%v] already exists\n", groupId)
+					log += fmt.Sprintf("ERROR - server, groupId [%v] already exists\n", groupId)
 					continue
 				}
 				
 				currGroups[groupId] = servers
-				
-				for i:=0;i<NShards;i++ {
-					if( i%(currGroupLen + 1) == 0) {
-						currShards[i] = groupId
-						log += fmt.Sprintf("server, assign shard [%v] to [%v]\n", i, groupId)
-					}	
-				}
 			}
 			
 			sm.NewConfig(currShards, currGroups)
+			if(len(sm.configs) == 2) {
+				sm.BalanceShardsForLeave()
+			} else {
+				sm.BalanceShardsForJoin()	
+			}
 			
 			joinReply.Err = Err_Success
 			joinReply.WrongLeader = false
@@ -228,27 +299,21 @@ func (sm *ShardMaster) ApplyToStateMachine(applyOp Op) (reply interface{}) {
 				_, ok := currGroups[gid]
 				
 				if(!ok) {
-					log += fmt.Sprintf("server, groupId [%v] not exists\n", gid)
+					log += fmt.Sprintf("ERROR - server, groupId [%v] not exists\n", gid)
 					continue
 				}
 				
 				delete(currGroups, gid)
 				
-				firstGid := 0
-				for id := range currGroups {
-					firstGid = id
-					break	
-				}
-			
-				for i:=1;i<NShards;i++ {
-					if( currShards[i] == gid) {
-						currShards[i] = firstGid
-						log += fmt.Sprintf("server, shard [%v] moves to [%v]\n", i, firstGid)
-					}	
+				for i:=0;i<len(currShards);i++ {
+					if(currShards[i] == gid) {
+						currShards[i] = 0
+					}
 				}
 			}
 			
 			sm.NewConfig(currShards, currGroups)
+			sm.BalanceShardsForLeave()
 			
 			leaveReply.Err = Err_Success
 			leaveReply.WrongLeader = false
@@ -267,7 +332,7 @@ func (sm *ShardMaster) ApplyToStateMachine(applyOp Op) (reply interface{}) {
 			if(!ok) {
 				moveReply.Err = Err_InvalidParameter
 				reply = moveReply
-				log += fmt.Sprintf("server, Move, groupId [%v] not exists\n", gid)
+				log += fmt.Sprintf("ERROR - server, Move, groupId [%v] not exists\n", gid)
 					
 				break	
 			}
@@ -275,7 +340,7 @@ func (sm *ShardMaster) ApplyToStateMachine(applyOp Op) (reply interface{}) {
 			if(shard < 0 || shard >= NShards) {
 				moveReply.Err = Err_InvalidParameter
 				reply = moveReply
-				log += fmt.Sprintf("server, Move, invalid shard [%v]\n", shard)
+				log += fmt.Sprintf("ERROR - server, Move, invalid shard [%v]\n", shard)
 					
 				break
 			}
@@ -283,7 +348,8 @@ func (sm *ShardMaster) ApplyToStateMachine(applyOp Op) (reply interface{}) {
 			currShards[shard] = gid
 			
 			sm.NewConfig(currShards, currGroups)
-			
+			// sm.BalanceShardsForJoin()
+						
 			moveReply.Err = Err_Success
 			moveReply.WrongLeader = false
 			
@@ -316,7 +382,7 @@ func (sm *ShardMaster) ApplyToStateMachine(applyOp Op) (reply interface{}) {
 	sm.configsMu.Unlock()
 	
 	if(sm.Leader) {
-		fmt.Printf(log)
+		fmt.Printf("  " + log)
 	}
 	
 	// fmt.Printf("#### %v End ApplyToStateMachine %v ####\n", sm.me, applyOp)
@@ -386,7 +452,8 @@ func (sm *ShardMaster) Join(args *JoinArgs, reply *JoinReply) {
 	}
 	
 	rr := r.(JoinReply)
-	reply = &rr
+	reply.Err = rr.Err
+	reply.WrongLeader = rr.WrongLeader
 	
 	return
 }
@@ -402,7 +469,8 @@ func (sm *ShardMaster) Leave(args *LeaveArgs, reply *LeaveReply) {
 	}
 	
 	rr := r.(LeaveReply)
-	reply = &rr
+	reply.Err = rr.Err
+	reply.WrongLeader = rr.WrongLeader
 	
 	return
 }
@@ -418,7 +486,8 @@ func (sm *ShardMaster) Move(args *MoveArgs, reply *MoveReply) {
 	}
 	
 	rr := r.(MoveReply)
-	reply = &rr
+	reply.Err = rr.Err
+	reply.WrongLeader = rr.WrongLeader
 	
 	return
 }
@@ -437,6 +506,7 @@ func (sm *ShardMaster) Query(args *QueryArgs, reply *QueryReply) {
 	// fmt.Printf("server, Query reply [%v]\n", rr)
 	reply.Config = rr.Config
 	reply.Err = rr.Err
+	reply.WrongLeader = rr.WrongLeader
 	
 	return
 }
